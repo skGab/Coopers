a1e373cf361f16f6b85374007801491993cff727
zDeep10 <gabriel.wti02@gmail.com>
2024-02-29 11:55:56 -0300

    api url fixes

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,12 +1,6 @@
 import axios from 'axios';
 
-let baseURL;
-
-if (process.env.NODE_ENV === 'production') {
-  baseURL = process.env.GO_API_URL;
-} else {
-  baseURL = 'http://localhost:8080';
-}
+const baseURL = import.meta.env.VITE_GO_API_URL;
 
 const api = axios.create({
   baseURL: baseURL,
